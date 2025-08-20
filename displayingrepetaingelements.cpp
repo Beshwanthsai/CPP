c409b9169d10d0fdee30824b9d3146151991ab0f
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-08-20 16:05:25 +0530

    learnt how to find the repeating elements in the array and displaying it

---
+++ displayingrepetaingelements.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"enter size of array: ";
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"repeating elements are:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        
+        {
+            if(a[i]==a[j])
+            {
+                cout<<a[i]<<endl;
+            }
+        }
+    }
+
+}
