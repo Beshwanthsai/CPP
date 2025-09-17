a09fa992ad11ffd5c5b4447b534999a2e411883a
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-09-17 07:48:42 +0530

    found the score of a given string leetcode 3110

---
+++ score_of_string.cpp
@@ -0,0 +1,23 @@
+/*
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+*/
+
+#include<iostream>
+#include<string>
+#include<cmath> // for abs function
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    int score = 0;
+    for(int i=0;i<s.length()-1;i++)
+    {
+        score += abs(s[i]-s[i+1]);
+    }
+    cout<<score;
+    
+    return 0;
+}
