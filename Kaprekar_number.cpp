5aead32fbbbc71d91f0fd10838390004e4ed5024
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-09-18 07:59:56 +0530

    Kaprekar Number

---
+++ Kaprekar_number.cpp
@@ -0,0 +1,25 @@
+// write a program to check whether a number is a kaprekar number or not
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+bool isKaprekar(int n)
+{
+    int digits = (int)log10(n)+1;
+    int square = (long long)n*n;
+    int topow = (int)pow(10,digits);
+    int right = square % topow;
+    int left = square / topow;
+    if(left + right == n)
+        return true;
+    return false;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    if(isKaprekar(n))
+        cout<<n<<" is a Kaprekar number.";
+    else
+        cout<<n<<" is not a Kaprekar number.";
+}
