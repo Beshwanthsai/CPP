0c64458c2e75de9c842a7e69dc7ce36214745c06
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-12-03 17:53:44 +0530

    min max element in an array

--- Arrays/reevv.cpp
+++ Arrays/reevv.cpp
@@ -7,17 +7,26 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"enter number of array elements u want: ";
     cin>>n;
     vector<int> a(n);
-
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    reverse(a.begin(), a.end());
+    int max=a[0];
+    int min=a[0];
     for(int i=0;i<n;i++)
     {
-        cout<<a[i]<<" ";
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+        if(a[i]<min)
+        {
+            min=a[i];
+        }
     }
+    cout<<"Maximum element is: "<<max<<endl;
+    cout<<"Minimum element is: "<<min<<endl;
+    return 0;
 }
