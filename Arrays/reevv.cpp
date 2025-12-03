cbcf1a01a870d75c0375baa3c1d8991eda05e750
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-12-03 20:29:16 +0530

    frequncy checkign using unorderd_map

--- Arrays/reevv.cpp
+++ Arrays/reevv.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 
 using namespace std;
 
@@ -8,25 +9,17 @@ int main()
 {
     int n;
     cin>>n;
-    vector<int> a(n);
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    unordered_map<int, int> freq;
     for(int i=0;i<n;i++)
     {
-        cin>>a[i];
+        freq[arr[i]]++;
     }
-    int max=a[0];
-    int min=a[0];
-    for(int i=0;i<n;i++)
+    for(auto it: freq)
     {
-        if(a[i]>max)
-        {
-            max=a[i];
-        }
-        if(a[i]<min)
-        {
-            min=a[i];
-        }
+       cout<<it.first<<" "<<it.second<<endl;
     }
-    cout<<"Maximum element is: "<<max<<endl;
-    cout<<"Minimum element is: "<<min<<endl;
     return 0;
 }
