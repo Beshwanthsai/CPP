61e17d45296752930470fe4ee0b002ae5b64dda8
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-12-22 15:25:40 +0530

    reversing the string without reverse function

---
+++ StringOperations/reversewithoutrev.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int left = 0, right =s.length()-1;
+    while(left<right)
+    {
+        swap(s[left],s[right]);
+        left++; right--;
+    }
+    cout<<s;
+}
