edab4a4be86faf48c1c14cad63fb29a22c05ed60
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-12-22 15:29:38 +0530

    reversing the string without reverse function and checking whether it is a palindrome or not

---
+++ StringOperations/palindromestring.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    string ss = s;
+    int left = 0, right =s.length()-1;
+    while(left<right)
+    {
+        swap(s[left],s[right]);
+        left++; right--;
+    }
+    if(s==ss)
+    {
+        cout<<"Palindrome"<<endl;
+    }
+    else{
+        cout<<"Not a Palindrome"<<endl;
+    }
+}
