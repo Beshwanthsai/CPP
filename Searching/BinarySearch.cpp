85c1ba38b3b14ea065fb333ba63633e3258e3ce3
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-12-24 10:23:22 +0530

    binary search

---
+++ Searching/BinarySearch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    int left = 0, right = n-1;
+    while(left <= right)
+    {
+        int mid = (left+right)/2;
+        if(target == arr[mid])
+        {
+            cout<<"element found at index :"<<mid<<endl;
+            return 0;
+        }
+        else if(target < arr[mid])
+        {
+            right = mid -1;
+        }
+        else {
+            left = mid +1;
+        }
+    }
+}
