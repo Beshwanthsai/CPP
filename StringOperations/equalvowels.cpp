88a83b50035e4977f6905a6a4112f2f59a1a91fb
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-12-31 22:52:59 +0530

    equal num of arrays in the substring of an array

---
+++ StringOperations/equalvowels.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n = s.length();
+        int mid = n / 2;
+        int co1 = 0, co2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'
+             || s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U') {
+
+                if (i < mid)
+                    co1++;
+                else
+                    co2++;
+            }
+        }
+        return co1 == co2;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string s;
+    cin >> s;  
+
+    cout << (sol.halvesAreAlike(s) ? "true" : "false") << endl;
+
+    return 0;
+}
