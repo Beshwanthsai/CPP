6b0779fd73246d61a4941b056b40e3f39bba8817
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-01 19:33:50 +0530

    character count checkign for a specific character

---
+++ StringOperations/charcount2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    int count=0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]=='a'||s[i]=='A')
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
