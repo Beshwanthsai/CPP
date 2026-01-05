9aad59940c9509e7b7e1f603b39abb64be3570ef
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-05 22:57:33 +0530

    structure pass by reference updation

---
+++ LinkedList/updatingstructure.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int id;
+    string name;
+    float marks;
+}s;
+
+void updatemarks(Node &s)
+{
+    s.marks = s.marks + 10;
+    s.name =s.name + "Updated";
+
+}
+
+int main()
+{
+    cin>>s.id;
+    cin>>s.name;
+    cin>>s.marks;
+    updatemarks(s);
+    cout<<s.id<<endl;
+    cout<<s.name<<endl;
+    cout<<s.marks<<endl;
+}   
