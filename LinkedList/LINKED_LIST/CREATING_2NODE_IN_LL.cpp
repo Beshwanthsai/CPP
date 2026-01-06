29570ffbc163f23a34f249460d95dce6ea1c9521
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-06 11:55:11 +0530

    created linked list in and two nodes and traversing in linked list

---
+++ LinkedList/LINKED_LIST/CREATING_2NODE_IN_LL.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+}s;
+
+int main()
+{
+    // Node* head = new Node();
+    // head->data = 10;
+    // head->next = new Node();
+    // head->next->data=20;
+    // head->next->next = NULL;
+
+    // cout<<head->data<<endl;
+    // cout<<head->next->data<<endl;
+    // cout<<head->next->next<<endl;
+    // return 0;
+
+
+    Node* first = new Node();
+    Node* second = new Node();
+
+    first -> data =10;
+    first -> next = second;
+    second -> data = 20;
+    second-> next = NULL;
+    cout<<first->data<<endl;
+    cout<<first->next->data<<endl;
+    cout<<second->data<<endl;
+    cout<<second->next<<endl;
+    return 0;
+
+}
