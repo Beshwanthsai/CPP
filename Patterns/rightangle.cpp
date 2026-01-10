c0637c46efd3b912dbaf1579e85aafdc7f614260
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-10 09:56:06 +0530

    right angle triangle

--- Patterns/rightangle.cpp
+++ Patterns/rightangle.cpp
@@ -6,11 +6,11 @@ int main()
 {
     int n;
     cin >> n;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<=n;i++)
     {
-        for(int j=0;j<=i;i++)
+        for(int j=1;j<=i;j++)
         {
-            cout << "* ";
+            cout << j;
         }
         cout << "\n";
     }
