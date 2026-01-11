c5cc47983db011d4b1de2053d690909eeb55cabe
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-11 13:10:37 +0530

    this is the alphabet count

---
+++ Hashing/alphabetcount.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin,s);
+    int freq[26]={0};
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]>='a' && s[i]<='z')
+        {
+            freq[s[i]-'a']++;
+        }
+        else if(s[i]>='A' && s[i]<='Z')
+        {
+            freq[s[i]-'A']++;
+        }
+
+    }
+    for(int i=0;i<26;i++)
+    {
+        if(freq[i]>0)
+        {
+            char ch = 'a' + i;
+            cout<<ch<<" "<<freq[i]<<"\n";
+        }
+    }
+}
