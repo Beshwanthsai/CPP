0b40036a24bc3b64d931e4837c8bfb115f9a6d9d
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-13 19:51:35 +0530

    maximum element to its RIGHT

---
+++ Arrays/maximumelementtoitsRIGHT.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        int max_ele = -1;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]>max_ele)
+            {
+                max_ele = a[j];
+            }
+        }
+        cout<<max_ele<<" ";
+    }
+    
+}
