75f18d7c0438a8cec1a7f459e80f57c7c9c7405c
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-19 10:45:51 +0530

    setithbit

---
+++ BitManipulation/setithbiton.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int setIthBit(int n, int i)
+{
+    return (n | (1 << i));
+}
+int main()
+{
+    int n, i;
+    cin >> n >> i;
+    cout << setIthBit(n, i) << endl;
+}
