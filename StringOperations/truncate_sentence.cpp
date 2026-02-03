2f4416c7de3101dbbdf4af9c6c05b5761d194772
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-02-03 23:02:57 +0530

    Add main function with test cases to truncate_sentence.cpp

--- StringOperations/truncate_sentence.cpp
+++ StringOperations/truncate_sentence.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <sstream>
+#include <iostream>
 using namespace std;
 
 class Solution {
@@ -19,3 +20,29 @@ public:
         return res;
     }
 };
+
+int main() {
+    Solution sol;
+    
+    // Test case 1
+    string s1 = "Hello how are you Contestant";
+    int k1 = 4;
+    cout << "Input: s = \"" << s1 << "\", k = " << k1 << endl;
+    cout << "Output: \"" << sol.truncateSentence(s1, k1) << "\"" << endl;
+    cout << endl;
+    
+    // Test case 2
+    string s2 = "What is the solution to this problem";
+    int k2 = 4;
+    cout << "Input: s = \"" << s2 << "\", k = " << k2 << endl;
+    cout << "Output: \"" << sol.truncateSentence(s2, k2) << "\"" << endl;
+    cout << endl;
+    
+    // Test case 3
+    string s3 = "chopper is not a tanuki";
+    int k3 = 5;
+    cout << "Input: s = \"" << s3 << "\", k = " << k3 << endl;
+    cout << "Output: \"" << sol.truncateSentence(s3, k3) << "\"" << endl;
+    
+    return 0;
+}
