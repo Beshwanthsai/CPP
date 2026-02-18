831dd012517c7263b7e0ef1063c7fa03bd901a77
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-02-18 16:43:07 +0530

    sorting

---
+++ SortingTechniques/sortingASC.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string> a(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+    
+}
