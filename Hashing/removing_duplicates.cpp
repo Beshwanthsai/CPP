ab89643e64b799fde5fb6deb8df79e22b688226f
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-02-26 10:02:30 +0530

    explaination

---
+++ Hashing/removing_duplicates.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>nums[i];
+    }
+    unordered_set<int> s;
+    for(int a:nums){.   // 1 2 3 4 5 1 2 3
+        s.insert(a);
+    }
+    for(int a:s)
+    {
+        cout<<a<<endl;
+    }
+    
+    
+}
