27e8db7ca913ac66addd5b480423eedf424fa0b6
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-03-02 10:01:21 +0530

    returning sub array

---
+++ Pointer-SlidingWindow/sub==k.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> nums={1,2,3,4,5};
+    int k = 3;
+    int left = 0;
+    int sum = 0;
+    for(int right = 0;right<nums.size();right++){
+        sum+=nums[right];
+        while (sum>k)
+        {
+            sum-=nums[left];
+            left++;
+        }
+        if(sum==k){
+            for(int i=left;i<right;i++){
+                cout<<nums[i]<< " ";
+            }
+            cout<<endl;
+        }
+        
+    }
+}
