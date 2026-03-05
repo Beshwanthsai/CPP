c28212f65922dc3c9ec13bf72fcfff363bd79cc4
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-03-05 09:59:19 +0530

    2d vector

---
+++ MatrixOperations/2dvector.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<vector<int>> mat = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int rows = mat.size();
+    int cols = mat[0].size();
+
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
