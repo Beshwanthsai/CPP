a753b9f550b9d1074110c06c18da3fd398b62095
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-03-13 15:28:34 +0530

    Grouping anagrams

---
+++ StringOperations/AnagramsGrouping.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> m;
+        for(int i=0;i<strs.size();i++){
+            string a = strs[i];
+            sort(a.begin(),a.end());
+            m[a].push_back(strs[i]);
+        }
+        vector<vector<string>> res;
+        for(auto &x:m){
+            res.push_back(x.second);
+        }
+}
+
+
+int main() {
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+
+    vector<vector<string>> res = groupAnagrams(strs);
+
+    for(auto group : res) {
+        for(string word : group)
+            cout << word << " ";
+        cout << endl;
+    }
+}
