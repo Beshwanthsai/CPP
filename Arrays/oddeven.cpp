d47a0a5b83655b8ee07590129e55a05d61b1a873
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-03-23 17:24:21 +0530

    pattern even odd # *

---
+++ Arrays/oddeven.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%2==0) cout<<"# ";
+        for(int j=1;j<=n-1;j++){
+            cout<<i<<" ";
+        }
+        if(i%2!=0) cout<<"* ";
+        cout<<endl;
+    }
+}
