186bc96b269fb4b54d1e6993bf9e76204521ef7d
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-03-24 22:53:47 +0530

    int reversal with out vector

--- Stacks/intreverse.cpp
+++ Stacks/intreverse.cpp
@@ -8,20 +8,22 @@ int main(){
     int n;
     cin>>n;
     stack<int> st;
-    vector<int> res;
+    // vector<int> res;
     for(int i=0;i<n;i++){
-        st.push(i);
+        int x;
+        cin>>x;
+        st.push(x);
     }
     if(st.empty()){
         cout<<"stack is empty"<<endl;
     }
     else{
         while(!st.empty()){
-            res.push_back(st.top());
+            cout<<st.top();
             st.pop();
         }
     }
-    for(int i=0;i<res.size();i++){
-        cout<<res[i]<<endl;
-    }
+    // for(int i=0;i<res.size();i++){
+    //     cout<<res[i]<<endl;
+    // }
 }
